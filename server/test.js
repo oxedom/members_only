72c41afe5d84258325e7e5dc9e74cf75068a845f
oxedom <oxedom@gmail.com>
2022-12-15 20:23:36 +0200

    deleted the mess

--- server/test.js
+++
@@ -1,6 +0,0 @@
-const queryUtils = require("../server/pg_utlis/query")
-
-
-// queryUtils.retrieveUserByID(4).then(data => console.log(data)).catch(err => console.log(err))
-
-queryUtils.retrieveUserByUsername('yuval123').then(data => console.log(data)).catch(err => console.log(err))
