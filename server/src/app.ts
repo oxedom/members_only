9d22021e2f980cb1619475b65640ca2c4403bc84
oxedom <oxedom@gmail.com>
2022-12-19 20:17:23 +0200

    moved to src

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-// import path from "path";
+import path from "path";
 import user_routes from "./routes/users_route";
 
 const app = express();
