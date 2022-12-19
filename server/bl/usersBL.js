5336801bc58984cd558a18460201c45e22a8cdb9
oxedom <oxedom@gmail.com>
2022-12-19 14:33:52 +0200

    added config

--- server/bl/usersBL.js
+++ server/bl/usersBL.js
@@ -2,19 +2,15 @@ const pool = require('../config')
 
 const getAllUsers = () => 
 {
-    const query = {
-        name: 'Fetch All users',
-        text: 'SELECT * FROM users'
-    }
 
     return new Promise((resolve, reject) => 
     {
         try {
-            res = pool.query(query, (err, res) => 
+            console.log(pool);
+            pool.query(query, (err, res) => 
             {
                 if(err) { reject(err)}
-                else { resolve(
-                    res.rows)}
+                else { resolve(res.rows)}
             })
         } catch (error) {
             reject(error)
