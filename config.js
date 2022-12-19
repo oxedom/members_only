76a2cb8a7a366a0bf0d91b45923477b540375d04
oxedom <oxedom@gmail.com>
2022-12-19 12:51:06 +0200

    removed src folder and moved fiels

--- config.js
+++
@@ -1,9 +0,0 @@
-
-const Pool = require('pg').Pool
-const pool = new Pool({
-    user:process.env.USER,
-    host: process.env.HOST,
-    database:process.env.DATABASE,
-    password:process.env.PASSWORD,
-    port: process.env.PORT,
-})
