1ec98c5c2eabec8f78c84d37c1a72a2dca7b807f
oxedom <oxedom@gmail.com>
2022-12-19 11:51:48 +0200

    working on config and env

---
+++ config.js
@@ -0,0 +1,8 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user:process.env.USER,
+    host: process.env.HOST,
+    database:process.env.DATABASE,
+    password:process.env.PASSWORD,
+    port: process.env.port,
+})
