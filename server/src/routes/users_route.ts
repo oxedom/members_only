9cdf26af868e23b831b4865ee265ccd58063910a
oxedom <oxedom@gmail.com>
2022-12-19 23:07:26 +0200

    messing with knex, trying to get config right

--- server/src/routes/users_route.ts
+++ server/src/routes/users_route.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response, NextFunction } from 'express'
-
+import connect from '../config'
 // import userBL from "../bl/usersBL"
 
 const router = express.Router()
 
 router.get('/users', (req: Request, res: Response, next: NextFunction) => {
   // let data = await userBL.getAllUsers();
+
   const data = ['TEST ']
   return res.json(data)
 })
